e0eaba2e57b573cdf2c375c5e74b42ac0e25349c
soulbyte07 <soul3147@proton.me>
2026-06-06 16:29:50 +0530

    cloud resources: + Override `equals()` and `hashCode()` based on `resourceId`

--- src/main/java/minitf/core/CloudResource.java
+++ src/main/java/minitf/core/CloudResource.java
@@ -24,6 +24,31 @@ public String getProvider() {
 }
 
 
+// Override `equals()` and `hashCode()` based on `resourceId`
+@Override
+public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    CloudResource that = (CloudResource) o;
+    return resourceId.equals(that.resourceId);
+}
+
+@Override
+public int hashCode() {
+    return resourceId.hashCode();
+}
+
+// Override `toString()` for better readability
+@Override
+public String toString() {
+    return "CloudResource{" +
+            "resourceId='" + resourceId + '\'' +
+            ", resourceName='" + resourceName + '\'' +
+            ", provider='" + provider + '\'' +
+            '}';
+}
+
+
 
 
 
